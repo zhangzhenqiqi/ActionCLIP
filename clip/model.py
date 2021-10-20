63599f388096d3c670d00d35ad272a3faaec16da
10501001 <10501001@amax.cm.cluster>
2021-10-20 21:14:04 +0800

    conv3d

--- clip/model.py
+++ clip/model.py
@@ -458,7 +458,7 @@ def convert_weights(model: nn.Module):
     """Convert applicable model parameters to fp16"""
 
     def _convert_weights_to_fp16(l):
-        if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear)):
+        if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear, nn.Conv3d)):
             l.weight.data = l.weight.data.half()
             if l.bias is not None:
                 l.bias.data = l.bias.data.half()
